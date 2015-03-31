a0751bf8c507fe931ac97801eca704b03940eeb3
Alex Johnson <alex@tablelist.com>
2015-03-31 10:32:58 -0400

    Add content

--- js/controllers.js
+++ js/controllers.js
@@ -55,8 +55,107 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone',
 		p2 : "p2",
 		p3 : "p3",
 		link : "link",
-  	}];
-    
+  	},{
+		img : "images/thumbs/evertrue-amp.png",
+		link: "#post/evertrue-amp",
+		nameSmall : "EverTrue's",
+		nameLarge : "Portal"
+	},{
+		img : "images/thumbs/texture-channel.png",
+		link: "#post/texture-channel",
+		nameSmall : "Texture",
+		nameLarge :  "Channel"
+	},{
+		img : "images/thumbs/branding.png",
+		link: "#gallery/branding Branding",
+		nameSmall : "Design",
+	},{
+		img : "images/thumbs/texturetechnologies.png",
+		link: "#post/texturetechnologies TextureTechnologies",
+		nameSmall : "com",
+	},{
+		img : "images/thumbs/clintvalentine.png" ,
+		link: "#post/clintvalentine ClintValentine",
+		nameSmall : "com",
+	},{
+
+		img : "images/thumbs/linkedin-grader.png",
+		link: "#post/linkedin-grader",
+		nameSmall : "EverTrue\'s",
+		nameLarge : "LinkedIn Grader"
+	},{
+
+		img : "images/thumbs/texture-report.png" ,
+		link: "#post/texture-report",
+		nameSmall : "Texture Report.com",
+	},{
+
+		img : "images/thumbs/fire.png" ,
+		link: "#gallery/fire Fire",
+		nameSmall : "Photography",
+	},{
+
+		img : "images/thumbs/fitbit-challenge.png",
+		link: "#post/fitbit-challenge",
+		nameSmall : "EverTrue\'s",
+		nameLarge : "FitBit Challenge",
+
+	},{
+
+		img : "images/thumbs/ttc-training-videos.png" ,
+		link: "#post/ttc-training",
+		nameSmall : "TA.XT2",
+		nameLarge : "Training Videos",
+	},{
+
+		img : "images/thumbs/quadcopter.png" ,
+		link: "#post/quadcopter",
+		nameSmall : "Project",
+		nameLarge : "quadcopter",
+	},{
+
+		img : "images/thumbs/evertrue-email.png",
+		link: "#post/evertrue-email",
+		nameSmall : "EverTrue\'s",
+		nameLarge : "E-mail Campaigns",
+	},{
+
+		img : "images/thumbs/eventler.png",
+		link: "#post/eventler",
+		nameSmall : "EverTrue\'s",
+		nameLarge : "Eventler",
+	},{
+
+		img : "images/thumbs/firebrand-innovations.png",
+		link: "#post/firebrand-innovations",
+		nameSmall : "Firebrand",
+		nameLarge : "Innovations.com",
+	},{
+
+		img : "images/thumbs/salem-state.png",
+		link: "#post/salem-state",
+		nameSmall : "Salem State",
+		nameLarge : "Design"
+	},{
+
+		img : "images/thumbs/tetris.png",
+		link: "#post/tetris",
+		nameSmall : "Tetris",
+		nameLarge : "in Scheme"
+	},{
+
+		img : "images/thumbs/macro.png",
+		link: "#gallery/macro",
+		nameSmall : "Macro",
+		nameLarge : "Photography",
+	}];
+
+		// @TODO | Add:
+		// Tablelist.com
+		// Tablelist Admin
+		// Swarm Navigation
+
+			
   }]);
 
 appControllers.controller('PostCtrl', ['$scope', '$routeParams', 'Phone',
@@ -65,3 +164,44 @@ appControllers.controller('PostCtrl', ['$scope', '$routeParams', 'Phone',
 
     
   }]);
+
+
+// <li class="mix landscape"><a href="images/gallery/landscape-etna.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-etna.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-etna-2.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-etna-2.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-favignana.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-favignana.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-naxos.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-naxos.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-naxos-2.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-naxos-2.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-nevada.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-nevada.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-renfe.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-renfe.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-santorini.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-santorini.jpg" /></a></li>
+// <li class="mix landscape"><a href="images/gallery/landscape-waterfall.jpg" class="fancybox" rel="landscape" title=""><img src="images/thumbs/landscape-waterfall.jpg" /></a></li>
+
+// <li class="mix branding"><a href="images/gallery/branding-ttc-banner-better.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-banner-better.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-channel-flier.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-channel-flier.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-our-knowledge.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-our-knowledge.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-print-cifst.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-print-cifst.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-crunch.jpg" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-crunch.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-texture-analysis.jpg" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-texture-analysis.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/branding-ttc-world-leaders.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/branding-ttc-world-leaders.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/logo-firebrand.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/logo-firebrand.png" /></a></li>
+// <li class="mix branding"><a href="images/gallery/logo-texture-technologies.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/logo-texture-technologies.jpg" /></a></li>
+// <li class="mix branding"><a href="images/gallery/logo-video-conversations.png" class="fancybox" rel="branding" title=""><img src="images/thumbs/logo-video-conversations.png" /></a></li>
+
+// <li class="mix fire"><a href="images/gallery/fire-beaver.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-beaver.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-sword.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-sword.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-drew.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-drew.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-fan.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-fan.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-fan-2.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-fan-2.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-fan-3.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-fan-3.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-fan-4.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-fan-4.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-mushroom.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-mushroom.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-poi.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-poi.jpg" /></a></li>
+// <li class="mix fire"><a href="images/gallery/fire-trevor.jpg" class="fancybox" rel="fire" title="Wildfire 2013"><img src="images/thumbs/fire-trevor.jpg" /></a></li>
+
+// <li class="mix macro"><a href="images/gallery/macro-bread.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-bread.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-cheeto.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-cheeto.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-gel.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-gel.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-pepper.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-pepper.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-pill.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-pill.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-pill-2.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-pill-2.jpg" /></a></li>
+// <li class="mix macro"><a href="images/gallery/macro-syringe.jpg" class="fancybox" rel="macro" title="Texture Technologies Macro Photography"><img src="images/thumbs/macro-syringe.jpg" /></a></li>

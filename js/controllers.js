20ae52e87a0992f6cf6573f67fe0d2b0bcc0ee75
Alex Johnson <alex@tablelist.com>
2015-04-15 17:25:32 -0400

    Updated content

--- js/controllers.js
+++ js/controllers.js
@@ -18,15 +18,15 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		img : "images/alex.jpg",
 		title : "About <span>ME</span>",
 		summary : [
-			"Javascript Developer. Computer Science", 
-			"and Interactive Media from Northeastern", 
-			"University. Boston, MA."
+			"Javascript Developer. Computer", 
+			"Science and Interactive Media", 
+			"Major at Northeastern"
 		],
 		description : "<b>I'm Alex</b>. Graduated from Northeastern University in 2015 with a degree in Computer Science and Interactive Media. <b>Javascript Developer</b> by trade with experience in Marketing and UX. Currently working at Tablelist.",
-		links : {
-			github : "https://github.com/alexjohnson505",
-			demo : "alexjohnson.io",
-		}
+		// links : {
+		// 	github : "https://github.com/alexjohnson505",
+		// 	demo : "alexjohnson.io",
+		// }
   	},{
 		img : "images/assignment04.png",
 		title : "Assignment <span>MEAN</span>",
@@ -38,7 +38,7 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		description : "Assignment #04 for CS4550 Web Development.",
 		links : {
 			demo : "http://nodejs-johnsoncs4550.rhcloud.com/#/courses",
-			code : "https://github.com/alexjohnson505/node.js-authentication",
+			github : "https://github.com/alexjohnson505/node.js-authentication",
 		}
   	},{
 		img : "images/assignment03.png",
@@ -57,7 +57,7 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		},
 		description : "Assignment #02 for CS4550 Web Development.",
   	},{
-		img : "http://placehold.it/480x480/BDB69C",
+		img : "images/blue.png",
 		title : "Assignment <span>HTML</span>",
 		summary : ["A basic example of HTML", "Creating the DOM for a web page."],
 		links : {
@@ -67,13 +67,13 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
   	},{
 		img : "images/tablelist.png",
 		title : "Tablelist <span>.com</span>",
-		summary : ["Tablelist's homepage", "Instant access to nightclubs and lounges", "nightlife reservations from the browser"],
+		summary : ["Tablelist's homepage. Instant", "access to nightclubs and lounges", "nightlife reservation software"],
 		links : {
 			demo : "http://tablelist.com"
 		},
 		description : "As the web developer at Tablelist, I created an Angular web app for the home page. This project expanding into a reservation platform for customers.",
   	},{
-		img : "http://placehold.it/480x480/717D8C",
+		img : "images/blue.png",
 		title : "Tablelist <span>Admin</span>",
 		summary : ["The Tablelist Admin Portal"],
 		description : "A set of tools for data management and analytics. This platform is used interally for marketing and sales. It maintains data analytics for the sales team and support tools for customer service.",
@@ -83,11 +83,12 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		summary : [],
 		links : {
 			github : "http://github.com/data-visualization-capstone/web",
+			demo : "http://vent8225.dyndns.org/map.html",
 		},
-		description : "A data visualization project for merging city data with social data and user activity. As the lead developer for my Interactive Media Capstone team, I created a data visualization platform, and an abstraction layer over the Leaflet.js API/",
+		description : "A data visualization project for merging city data with social data and user activity. As the lead developer for my Interactive Media Capstone team, I created a data visualization platform, and an abstraction layer over the Leaflet.js API",
   	},{
   	},{
-		img : "http://placehold.it/480x480/DDDDDD",
+		img : "images/livingcitiesapi.png",
 		title : "Living Cities <span>API</span>",
 		summary : [],
 		links : {
@@ -101,16 +102,15 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		description : "Working with the Game Design department of Northeastern, I was the lead developer for a web app for visualizing and analyzing player activity in video games. This tool was used in a study of player behaviour in open world video games.",
 		links : {
 			demo : "http://g-player.herokuapp.com",
-			online : "https://github.com/spacial-player-data-visualization/g-player-visualization",
+			github : "https://github.com/spacial-player-data-visualization/g-player-visualization",
 		},
   	}, {
 		img : "images/swarm.png",
 		title : "Swarm <span>Navigation</span>",
 		summary : ["Interactive Visualization", "of Swarm Behaviour using Craig", "Reynold's research in swarm algorithms."],
 		links : {
-			code : "https://github.com/alexjohnson505/swarm-navigation",
+			github : "https://github.com/alexjohnson505/swarm-navigation",
 			demo : "http://alexjohnson.io/swarm-navigation",
-			// @TODO Fix
 		},
   	}, {
 		img : "images/thumbs/evertrue-amp.png",
@@ -125,23 +125,29 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		summary : ["Subscription based", "video training resource", "for customers."],
 		description : "TextureChannel.com is a Subscription service offered by Texture Technologies as a solution to remote video training. Users are issued an account, where they can view training videos for the TA.XT2 Plus and Texture Measurement Software.",
 	},{
-		img : "images/thumbs/branding.png",
-		link: "#gallery/branding",
-		title : "Branding <span>Design</span>",
-		summary : ["Branding and", "marketing experience"],
-		description : "A quick overview of some branding and graphic design work I've created.",
-	},{
+	// 	img : "images/thumbs/branding.png",
+	// 	link: "#gallery/branding",
+	// 	title : "Branding <span>Design</span>",
+	// 	summary : ["Branding and", "marketing experience"],
+	// 	description : "A quick overview of some branding and graphic design work I've created.",
+	// },{
 		img : "images/thumbs/texturetechnologies.png",
 		link: "#post/texturetechnologies",
 		title : "Texture <span>Technologies.com</span>",
 		summary : ["Texture Technologies' Home Page", "Scientific Instrument and", "Texture Measurement support resources" ],
 		description : "TextureTechnologies.com is the primary resource for Texture Technologies' texture measurement hardware and software. Containing application studies, and hardware specifications.",
+		links : {
+			demo : "http://texturetechnologies.com/",
+		}
 	},{
 		img : "images/thumbs/clintvalentine.png" ,
 		link: "#post/clintvalentine ",
 		title : "Clint <span>Valentine.com</span>",
 		summary : ["Adventurer and Photography", "Personal site and blog."],
 		description : "ClintValentine.com is a personal site for Clint - an experienced rock climber, and cross country biker. Clint documents his travels along with photographing the environment.",
+		links : {
+			demo : "http://www.clintvalentine.com/",
+		}
 	},{
 
 		img : "images/thumbs/linkedin-grader.png",
@@ -157,63 +163,68 @@ appControllers.controller('HomeCtrl', ['$scope', 'Phone', '$sce',
 		summary : ["Texture research blog"],
 		description : "Texture Technologies' blog is a one-stop source for news in the world of texture analysis",
 	},{
-		img : "images/thumbs/fire.png" ,
-		link: "#gallery/fire",
-		title : "Fire <span>Photography</span>",
-		description : "",
-	},{
+	// 	img : "images/thumbs/fire.png" ,
+	// 	link: "#gallery/fire",
+	// 	title : "Fire <span>Photography</span>",
+	// 	description : "",
+	// },{
 		img : "images/thumbs/fitbit-challenge.png",
 		link: "#post/fitbit-challenge",
 		title : "FitBit <span>Challenge</span>",
-		description : "",
+		description : "A single page web app for tracking coworker's pedometer activity through the FitBit API. Race to 1,000,000 steps.",
 	},{
 		img : "images/thumbs/ttc-training-videos.png" ,
 		link: "#post/ttc-training",
 		title : "TA.XT2 <span>Training Videos</span>",
-		description : "",
+		description : "Filmed and edited over 20 hours of tutorial videos for Texture Analyzer TA.XT2 Hardware Training in addition to Texture Exponennt Software Training",
 	},{
 
 		img : "images/thumbs/quadcopter.png" ,
 		link: "#post/quadcopter",
 		title : "Project <span>quadcopter</span>",
-		description : "",
+		summary : ["An introduction into", "Arduino and C++"],
+		description : "As summer challenge, I worked with Nate Lilienthal to build a remote controllable Arduino-powered Quadcopter from scratch.",
 	},{
 
-		img : "images/thumbs/evertrue-email.png",
-		link: "#post/evertrue-email",
-		title : "Email <span>Campaigns</span>",
-		description : "",
-	},{
+	// 	img : "images/thumbs/evertrue-email.png",
+	// 	link: "#post/evertrue-email",
+	// 	title : "Email <span>Campaigns</span>",
+	// 	description : "",
+	// },{
 
-		img : "images/thumbs/eventler.png",
-		link: "#post/eventler",
-		title : "EverTrue\'s <span>Eventler</span>",
-		description : "",
-	},{
+	// 	img : "images/thumbs/eventler.png",
+	// 	link: "#post/eventler",
+	// 	title : "EverTrue\'s <span>Eventler</span>",
+	// 	description : "",
+	// },{
 
 		img : "images/thumbs/firebrand-innovations.png",
 		link: "#post/firebrand-innovations",
 		title : "Firebrand <span>Innovations.com</span>",
+		summary : ["Poster website for a", "video conferencing tech", "startup"],
 		description : "",
-	},{
-
-		img : "images/thumbs/salem-state.png",
-		link: "#post/salem-state",
-		title : "Salem State <span>Design</span>",
-		description : "",	
+		links : {
+			demo : "http://firebrandinnovations.com/",
+		}
 
 	},{
 
+	// 	img : "images/thumbs/salem-state.png",
+	// 	link: "#post/salem-state",
+	// 	title : "Salem State <span>Design</span>",
+	// 	description : "",	
+
+	// },{
 		img : "images/thumbs/tetris.png",
 		link: "#post/tetris",
 		title : "Tetris <span>in Scheme</span>",
-		description : "",
-	},{
+		description : "As a challenge, I re-built Tetris in Scheme.",
+	// },{
 
-		img : "images/thumbs/macro.png",
-		link: "#gallery/macro",
-		title : "Macro <span>Photography</span>",
-		description : "",
+	// 	img : "images/thumbs/macro.png",
+	// 	link: "#gallery/macro",
+	// 	title : "Macro <span>Photography</span>",
+	// 	description : "",
 	}];
 
 	angular.forEach($scope.posts, function(post){
